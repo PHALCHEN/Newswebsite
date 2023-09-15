242196988c6e9aed335019723e76bb1bc30246e7
PHALCHEN <119691980+PHALCHEN@users.noreply.github.com>
2023-09-15 16:29:38 +0700

    Add files via upload

---
+++ sport.js
@@ -0,0 +1,60 @@
+console.log("connected");
+
+function postNews(event) {
+  event.preventDefault(); // prevent reload page
+  let storage = JSON.parse(localStorage.getItem("news"));
+  let form = event.target;
+  let data = {
+    title: form.title.value,
+    cover: form.cover.value,
+    websiteUrl: form.websiteUrl.value,
+    postedAt: form.postedAt.value,
+    description: form.description.value,
+  };
+  console.log(data);
+  if (!storage) {
+    localStorage.setItem("news", JSON.stringify([data]));
+  } else {
+    storage.push(data);
+    localStorage.setItem("news", JSON.stringify(storage));
+  }
+  document.getElementById("closePost").click();
+  displayUi();
+}
+
+function displayUi() {
+  let postUi = document.getElementById("posts");
+  let storage = JSON.parse(localStorage.getItem("news"));
+  if (!storage) {
+    postUi.innerHTML = "No post, please a news";
+  } else {
+    let tmpContent = "";
+    for (let i = 0; i < storage.length; i++) {
+      tmpContent += `
+    <div class="col-3">    
+    <div class="newsBox">
+    <div class="newsCard" style="width: 18rem;">
+      <div class="img">
+          <img src="${storage[i].cover}" alt="cover">
+      </div>
+      <div class="text"> ${storage[i].title}
+        <div class="description"> ${storage[i].description}
+          <div class="title">
+              <a href="${storage[i].websiteUrl}" target="_blank" >
+              <button type="button" class="btn btn-primary btn-sm">
+              See more..
+            </button>
+              </a>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div> </div>
+ 
+
+`;
+    }
+    postUi.innerHTML = tmpContent;
+  }
+}
+displayUi();
--- topheadlines.js
+++ topheadlines.js
@@ -2,7 +2,7 @@ console.log("connected");
 
 function postNews(event) {
   event.preventDefault(); // prevent reload page
-  let storage = JSON.parse(localStorage.getItem("breakingNew"));
+  let storage = JSON.parse(localStorage.getItem("mixed"));
   let form = event.target;
   let data = {
     cover: form.cover.value,
@@ -12,39 +12,39 @@ function postNews(event) {
   };
   console.log(data);
   if (!storage) {
-    localStorage.setItem("breakingNew", JSON.stringify([data]));
+    localStorage.setItem("mixed", JSON.stringify([data]));
   } else {
     storage.push(data);
-    localStorage.setItem("breakingNew", JSON.stringify(storage));
+    localStorage.setItem("mixed", JSON.stringify(storage));
   }
   document.getElementById("closePost").click();
   displayUi();
 }
 
 function displayUi() {
-  let postUi = document.getElementById("postBreaking");
-  let storage = JSON.parse(localStorage.getItem("breakingNew"));
+  let postUi = document.getElementById("postTopheadlines");
+  let storage = JSON.parse(localStorage.getItem("mixed"));
   if (!storage) {
     postUi.innerHTML = "No post, please a news";
   } else {
     let tmpContent = "";
     for (let i = 0; i < storage.length; i++) {
       tmpContent += `
-
-    <div class="img" id="breakingImg">
-    <img src="${storage[i].cover}" alt="cover">
-    </div>
-    <div class="text" id="breakingNews">
-    <div class="description"> ${storage[i].description}
-    <div class="title">
-        <a href="${storage[i].websiteUrl}" target="_blank" >
-        <button type="button" class="btn btn-primary btn-sm">
-        See more..
-      </button>
-        </a>
+      <div class="news">
+      <div class="img">
+        <img src="${storage[i].cover}" alt="cover">
       </div>
-  </div>
+      <div class="description"> ${storage[i].description}
+        <div class="title">
+            <a href="${storage[i].websiteUrl}" target="_blank" >
+            <button type="button" class="btn btn-primary btn-sm">
+            See more..
+          </button>
+            </a>
+          </div>
+      </div>
     </div>
+
 `;
     }
     postUi.innerHTML = tmpContent;
